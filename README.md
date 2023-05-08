d336ad4e934994d299fd06c1df4392b69f720a87
Anila <ghasemienila@gmail.com>
2023-05-08 14:21:16 +0200

    Erledigt

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Portfolio
 ![alt text](images/Readme.png)
+
+Dies ist ein HTML-Code für meine Portfolio-Website (Anila Ghasemi).
+
+Ich habe HTML, CSS und ein wenig JavaScript auf dieser Seite verwendet.
+
+Wie ich schon erwähnt habe, wird die Website mit dem MERN-Stack erstellt.
--- js/script.js
+++ js/script.js
@@ -1,36 +1,43 @@
-let menu = document.querySelector('#menu-bars');
-let header = document.querySelector('header');
+let menu = document.querySelector("#menu-bars");
+// let header = document.querySelector('header');
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    header.classList.toggle('active');
-}
+// menu.onclick = () =>{
+//     menu.classList.toggle('fa-times');
+//     header.classList.toggle('active');
+// }
 
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    header.classList.remove('active');
-}
+// window.onscroll = () =>{
+//     menu.classList.remove('fa-times');
+//     header.classList.remove('active');
+// }
 
-let cursor1 = document.querySelector('.cursor-1');
-let cursor2 = document.querySelector('.cursor-2');
+// Der kümmert sich um die Erstellung von zwei animierten Cursors auf der Webseite.
+//Der Cursor besteht aus zwei Elementen mit den Klassen cursor-1 und cursor-2,
+//die sich beim Bewegen der Maus auf der Webseite folgen.
 
-window.onmousemove = (e) =>{
-    cursor1.style.top = e.pageY + 'px';
-    cursor1.style.left = e.pageX + 'px';
-    cursor2.style.top = e.pageY + 'px';
-    cursor2.style.left = e.pageX + 'px';
-}
+let cursor1 = document.querySelector(".cursor-1");
+let cursor2 = document.querySelector(".cursor-2");
 
-document.querySelectorAll('a').forEach(links =>{
+//Das window.onmousemove Ereignis wird verwendet, um die Position der beiden Cursor-Elemente an die
+//Mausposition anzupassen. Wenn der Mauszeiger über einem Link schwebt,
+//werden beide Cursor-Elemente durch das Hinzufügen der Klasse active animiert.
+//Wenn der Mauszeiger den Link verlässt, wird die Klasse active von beiden Cursor-Elementen entfernt.
 
-    links.onmouseenter = () =>{
-        cursor1.classList.add('active');
-        cursor2.classList.add('active');
-    }
+window.onmousemove = (e) => {
+  cursor1.style.top = e.pageY + "px";
+  cursor1.style.left = e.pageX + "px";
+  cursor2.style.top = e.pageY + "px";
+  cursor2.style.left = e.pageX + "px";
+};
 
-    links.onmouseleave = () =>{
-        cursor1.classList.remove('active');
-        cursor2.classList.remove('active');
-    }
+document.querySelectorAll("a").forEach((links) => {
+  links.onmouseenter = () => {
+    cursor1.classList.add("active");
+    cursor2.classList.add("active");
+  };
 
+  links.onmouseleave = () => {
+    cursor1.classList.remove("active");
+    cursor2.classList.remove("active");
+  };
 });
